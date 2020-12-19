06e457f336d22db80a029d40eda037cc0ee08479
FlavoursomeCoffee <72069436+FlavoursomeCoffee@users.noreply.github.com>
2020-12-19 18:10:10 +1100

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ app.get('/', (req, res) => res.send('Hello there! Click this link if you want to
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 // ================= START BOT CODE ===================
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello there! Click this link if you want to add my bot to your server! https://discord.com/oauth2/authorize?client_id=730325671284572200&scope=bot&permissions=8'));
+
+app.listen(port, () => console.log(`Minecraft Block Bot listening at http://localhost:${port}`));
+
+// ================= START BOT CODE ===================
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -33,7 +52,9 @@ client.on('message', msg => {
           icon_url: client.user.avatarURL
         },
         title: "**Granite**",
-        url: "https://static.wikia.nocookie.net/minecraft/images/0/0b/PolishedGraniteNew.png/revision/latest?cb=20190903225756",
+        image: {
+		      url: 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/7/7e/Granite_JE2_BE2.png/revision/latest/scale-to-width-down/150?cb=20200315183719',
+	      },
         description: "Granite is a block in Minecraft.",
         fields: [{
           name: "Spawning",
@@ -44,15 +65,17 @@ client.on('message', msg => {
         timestamp: new Date(),
         footer: {
           icon_url: client.user.avatarURL,
-          text: "© Mojang"
+          text: "This is called text"
         }
       }
     });
+} else if (msg.content === `${prefix}beep`) {
+	msg.reply('Boop.');
 
-  } else if (msg.content.startsWith(prefix + "beep")) {
-    msg.reply('boop');
-  }
+} else if (msg.content === `${prefix}konami`) {
+  msg.reply('Up Up Down Down Left Right Left Right B A Start')   
+ }
 });
 // 
 client.login(process.env.DISCORD_TOKEN);
-client.login('Secret Token')
+client.login('Secret')
